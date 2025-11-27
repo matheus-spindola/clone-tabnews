19dabc3bd1fbe66941f9097ec97597d301736ec5
Matheus Spindola <matheus@crosbyhealth.com>
2025-11-27 15:00:09 +0000

    add pages

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return <h1>O Paçoca é gordinho <br></br>
+        A Loló pequenininha<br></br>
+        Se você me ama <br></br>
+        Dá uma risadinha <br></br>
+        XD
+    </h1>
+}
+
+export default Home;
