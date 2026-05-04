3e728cb7fbb585e35e803080717892d5e1602e88
Matheus Spindola <matheus@crosbyhealth.com>
2026-05-04 20:23:20 +0000

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  response.status(200).json({ message: "Status 200 - OK" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  response.status(200).json({ chave: "Status 200 - OK" });
 }
 
 export default status;
